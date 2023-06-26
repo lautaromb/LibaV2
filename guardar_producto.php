4cc7727604b739e6b7e80b35577ffe46dcf53c71
Lautaro <lmmb96@gmail.com>
2023-06-25 21:26:47 -0300

    cambios y actualizacion en productos

--- guardar_producto.php
+++ guardar_producto.php
@@ -1,9 +1,35 @@
 <?php
-?>
-<?php
-if (!isset($_POST["nombre"]) || !isset($_POST["precio"]) || !isset($_POST["descripcion"])) {
+// Verificar si se enviaron todos los datos necesarios
+if (!isset($_POST["nombre"]) || !isset($_POST["precio"]) || !isset($_POST["descripcion"]) || !isset($_FILES["img"])) {
     exit("Faltan datos");
 }
+
 include_once "funciones.php";
-guardarProducto($_POST["nombre"], $_POST["precio"], $_POST["descripcion"]);
-header("Location: productos.php");
+
+// Obtener los datos del formulario
+$nombre = $_POST["nombre"];
+$descripcion = $_POST["descripcion"];
+$precio = $_POST["precio"];
+$imagen = $_FILES["img"];
+
+
+// Ruta donde se guardará la imagen
+$directorioDestino = 'img/productos/'; // Reemplaza con la ruta deseada
+
+// Verificar si se seleccionó una imagen
+if ($imagen["error"] === UPLOAD_ERR_OK) {
+    $nombreArchivo = $imagen["name"];
+    $rutaArchivo = $directorioDestino . $nombreArchivo;
+    // Mover la imagen al directorio de destino
+    if (move_uploaded_file($imagen["tmp_name"], $rutaArchivo)) {
+        // Guardar la información del producto en la base de datos
+        guardarProducto($nombre, $descripcion, $precio, $rutaArchivo);
+        header("Location: productos.php");
+        exit;
+    } else {
+        exit("Error al mover el archivo de imagen");
+    }
+} else {
+    exit("Error al subir la imagen");
+}
+?>
